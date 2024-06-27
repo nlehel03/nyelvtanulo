92b7e576f3bee2f3bf042113c38c7bd33ba9e60c
nlehel03 <nlehel03@gmail.com>
2024-06-27 13:27:00 +0200

    2024.06.27

---
+++ nyelvtanulas/Szotar.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nyelvtanulas
+{
+    internal class Szotar
+    {
+        public Dictionary<string, string> szavak;
+        public Szotar()
+        {
+            szavak=new Dictionary<string, string>();
+        }
+        public void feltolt(string kulcsnyelv, params string[] fajlok)
+        {
+            if(kulcsnyelv=="angol")
+            {
+                foreach(string item in fajlok)
+                {
+                    using (StreamReader sr = new StreamReader(item))
+                        while (!sr.EndOfStream)
+                    {
+                        string[] szopar=sr.ReadLine().Split(' ');
+                        if (!szavak.ContainsKey(szopar[0]) && szopar.Length == 2)
+                        {
+                            szavak.Add(szopar[0], szopar[1]);
+                        }
+                    }
+                }
+            }
+            else if(kulcsnyelv=="magyar")
+            {
+                foreach (string item in fajlok)
+                {
+                    using (StreamReader sr = new StreamReader(item))
+                        while (!sr.EndOfStream)
+                        {
+                            string[] szopar = sr.ReadLine().Split(' ');
+                            if (!szavak.ContainsKey(szopar[1])&&szopar.Length==2)
+                            {
+                                szavak.Add(szopar[1], szopar[0]);
+                            }
+                        }
+                }
+            }
+            else
+            {
+                throw new Exception("nem támogatott nyelv: "+kulcsnyelv);
+            }
+        }
+
+        public void szoTorles(string kulcs)
+        {
+            szavak.Remove(kulcs);
+        }
+
+        public void temaTorles(string kulcsnyelv, params string[] fajlok)
+        {
+            if (kulcsnyelv == "angol")
+            {
+                foreach (string item in fajlok)
+                {
+                    using (StreamReader sr = new StreamReader(item))
+                        while (!sr.EndOfStream)
+                    {
+                        string[] szopar = sr.ReadLine().Split(' ');
+                        szavak.Remove(szopar[0]);
+                    }
+                }
+            }
+            else if (kulcsnyelv == "magyar")
+            {
+                foreach (string item in fajlok)
+                {
+                    using (StreamReader sr = new StreamReader(item))
+                        while (!sr.EndOfStream)
+                        {
+                            string[] szopar = sr.ReadLine().Split(' ');
+                            szavak.Remove(szopar[1]);
+                        }
+                }
+            }
+            else
+            {
+                throw new Exception("nem támogatott nyelv: "+kulcsnyelv);
+            }
+        }
+
+    }
+}
